1491b3e735d7d982c2423e934552e2aa9786e6e8
tushar17s <tusharsaini1703@gmail.com>
2025-12-05 22:20:25 +0530

    my commit

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path , include
 from . import views
 urlpatterns = [
-    path('', views.home , name = 'first' ),
+    path('', views.home , name = 'home' ),
+    path('signup/', views.sign_up , name = 'signup' ),
+    path('login/', views.log_in , name = 'login' ),
+    path('logout/', views.log_out , name = 'logout' ),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User 
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout 
 # Create your views here.
 def home(request) :
-    return HttpResponse("hello world !")
+    return render(request , "base/home.html")
     
+def sign_up(request):
+    # info collection
+    if request.method == 'POST' :
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+    
+    # info validation
+    
+        if cpassword!=password :
+            messages.error(request,"password does not match, try again")
+            return render(request,"base/signup.html")
+        
+        if User.objects.filter(username=username).exists() :
+            messages.error(request,"username already exists")
+            return render(request,"base/signup.html")
+        
+        if User.objects.filter(email=email).exists() :
+            messages.error(request,"email already exists")
+            return render(request,"base/signup.html")
+            
+        #  create user or password hashing (comes with create_user)
+        
+        user  = User.objects.create_user(username=username , email=email , password=password)
+        user.save()
+        messages.success(request, "Account created! You can now login.")
+        logout(request)
+        return render(request,"base/home.html")
+        
+    return render(request,"base/signup.html")
+
+def log_in(request) :
+    if request.method == "POST" :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request , username=username , password = password)
+        # ye output None or not None me dega
+        if user is not None :
+            login(request,user)
+            messages.success(request,"logged in successfully!!")
+            return redirect("home")
+        else :
+            messages.error(request,"user not found!!")
+            return render(request,"base/signup.html")
+    else :
+        return render(request,"base/login.html")
+            
+def log_out(request) :
+    logout(request)
+    messages.success(request,"logged out successfully!!")
+    return redirect("home")
