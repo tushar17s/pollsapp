6a52ecba9df721ececc0e8a2981e0b090816d1bd
tushar17s <tusharsaini1703@gmail.com>
2025-12-06 23:04:28 +0530

    my commit

--- base/models.py
+++ base/models.py
@@ -1,4 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Poll(models.Model) :
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=10)
+    is_public = models.BooleanField(default=True)
     
+class PollOption(models.Model) :
+    option_text = models.TextField(max_length=100)
+    poll = models.ForeignKey(Poll,on_delete=models.CASCADE)
+    
+class Vote(models.Model):
+    poll = models.ForeignKey(Poll,on_delete=models.CASCADE)
+    option = models.ForeignKey(PollOption,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
